f50ca414d4fb6d975f3945c823a8ae4a006311d4
Arambse <drvender@drs-MacBook-Air.local>
2013-07-30 22:52:57 +0300

    fixed issue on movement of pages to detailed view. used $location reference in the parties and z-my-night controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,23 +12,29 @@ angular.module('Zuller')
   .controller('HomeCtrl', ['$scope', function($scope) {
     $scope.templateUrl = 'partials/home.html';
   }])
-  .controller('PartiesCtrl', ['$scope', '$http', function($scope, $http) {
+  .controller('PartiesCtrl', ['$scope', '$http', '$location', 'sharedProperties', function($scope, $http, $location, sharedProperties) {
     $scope.templateUrl = 'partials/parties.html';
+    $scope.onPartyClicked = function(party) {
+        sharedProperties.setCurrentParty(party);
+        $location.path('parties/' + party._id);
+      }
     $http({ method: 'JSONP', url: host + '/parties.js?callback=JSON_CALLBACK' }).success(function(data) {
       $scope.parties = data;
     });
   }])
-  .controller('ZullerMyNightCtrl', ['$scope', '$http', 'sharedProperties',function($scope, $http, sharedProperties) {
+  .controller('ZullerMyNightCtrl', ['$scope', '$http', '$location', 'sharedProperties',function($scope, $http, $location, sharedProperties) {
     $scope.templateUrl = 'partials/zuller-my-night.html';
     $http({ method: 'JSONP', url: host + '/zuller_my_night.js?callback=JSON_CALLBACK' }).success(function(data) {
       $scope.parties = data.parties;
       $scope.bars = data.bars;
       $scope.category = 'bars';
-      $scope.setCurrentParty = function(party){
+      $scope.onPartyClicked = function(party) {
         sharedProperties.setCurrentParty(party);
+        $location.path('parties/' + party._id);
       };
-      $scope.setCurrentBar = function(bar){
+      $scope.onBarClicked = function(bar) {
         sharedProperties.setCurrentBar(bar);
+        $location.path('bars/' + bar._id);
       };
     })
   }])
